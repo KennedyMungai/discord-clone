5f63ee1b1060a88febaf91b5b24e763ed81ed070
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-10 15:16:48 +0300

    Gave the page some mode specific background colors

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "@/components/ModeToggle";
 
 export default function Home() {
   return (
-    <main className='min-h-screen'>
+    <main className='min-h-screen bg-white dark:bg-[#313338]'>
       <a href="/api/auth/login">Login</a>
       <ModeToggle />
     </main>
