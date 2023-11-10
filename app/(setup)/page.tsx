8d322e5749e7dd21b96e4665805516a9beb5f8d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-10 15:59:43 +0300

    Moved the page tsx file

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,6 @@
 import { ModeToggle } from "@/components/ModeToggle";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className='min-h-screen bg-white dark:bg-[#313338]'>
       <a href="/api/auth/login">Login</a>
