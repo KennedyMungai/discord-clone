46d2a51963b38ca2c5587a4b504985e9785fffeb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-10 15:01:11 +0300

    Made some changes for the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className='min-h-screen'></main>
+    <main className='min-h-screen'>
+      <a href="/api/auth/login">Login</a>
+    </main>
   )
 }
