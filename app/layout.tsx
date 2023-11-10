5be7cbb1b9244d23a16e14cc3735f4903d6a397d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-10 12:35:52 +0300

    Added the UserProvider to the layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
+import UserProvider from '@/Providers/UserProvider'
 
 const open_sans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Discord Clone',
-  description: 'A simple discord clone',
+	title: 'Discord Clone',
+	description: 'A simple discord clone'
 }
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={open_sans.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<UserProvider>
+				<body className={open_sans.className}>{children}</body>
+			</UserProvider>
+		</html>
+	)
 }
