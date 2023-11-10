2cdd16a94e2a24228e5d403b6b5beceabd58b5a1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-10 15:14:26 +0300

    Added the ModeToggle component to the index of the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/ModeToggle";
+
 export default function Home() {
   return (
     <main className='min-h-screen'>
       <a href="/api/auth/login">Login</a>
+      <ModeToggle />
     </main>
   )
 }
