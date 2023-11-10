20c8ab43a37b692b3c58b7af3b6f4c1d80032dd9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-10 12:37:40 +0300

    Made some updated for the component

--- Providers/UserProvider.tsx
+++ Providers/UserProvider.tsx
@@ -3,15 +3,9 @@ import { UserProvider } from '@auth0/nextjs-auth0/client'
 import { ReactNode } from 'react'
 
 type Props = {
-    children: ReactNode
+	children: ReactNode
 }
 
 export default function RootLayout({ children }: Props) {
-	return (
-		<html lang='en'>
-			<UserProvider>
-				<body>{children}</body>
-			</UserProvider>
-		</html>
-	)
+	return <UserProvider>{children}</UserProvider>
 }
